02efbac4319be229ce02c06279cfde11eec46403
kavya <kavyadeliwala18@gmail.com>
2023-01-31 12:58:32 +0530

    Dashboard Responsive

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$('#toggalbtn').click(function () {
+    console.log("hello");
+     $(this).toggleClass("click");
+     if ($('#sidebarMenu').hasClass('xyz')) {
+         $('#sidebarMenu').removeClass("d-none xyz");
+         $('#sidebarMenu').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+         $('#mainContent').removeClass("col-lg-12");
+         $('#mainContent').addClass("col-lg-10");
+     } else {
+         $('#sidebarMenu').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+         $('#sidebarMenu').addClass("d-none xyz");
+         $('#mainContent').removeClass("col-lg-10");
+         $('#mainContent').addClass("col-lg-12");
+     }
+ });
